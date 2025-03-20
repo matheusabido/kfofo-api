b6078a891241b95a1822796c2909f1aaa13cce12
matheusabido <matheusabidux@gmail.com>
2025-03-20 17:25:06 -0400

    teste

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/matheusabido/kfofo-api/db"
 )
 
@@ -8,6 +11,7 @@ func main() {
 	// if err := godotenv.Load(); err != nil && os.Getenv("OCI_PRIVATE_KEY_BASE64") == "" {
 	// 	log.Fatal("Failed to load .env")
 	// }
+	fmt.Println(os.Getenv("DB_URL"))
 
 	db.SetupDB()
 	SetupRoutes()
